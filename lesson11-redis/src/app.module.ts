4d666516b5b70c0f05ac5953228f8b826fef269f
山人自有妙计 <wangdaoo@yeah.net>
2024-05-21 15:54:39 +0800

    feat: redis :tada:

--- lesson11-redis/src/app.module.ts
+++ lesson11-redis/src/app.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CacheModule } from '@nestjs/cache-manager';
 import { UsersModule } from './users/users.module';
-// import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [
-    // 本地缓存
-    CacheModule.register({
-      ttl: 300,
-      max: 100,
-      isGlobal: true,
-    }),
-    UsersModule,
-  ],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
