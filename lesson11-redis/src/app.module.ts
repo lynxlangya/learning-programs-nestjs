33b68e7fae92a8066841e25b585979e155f6b8d1
山人自有妙计 <wangdaoo@yeah.net>
2024-05-21 17:08:28 +0800

    feat: redis :tada:

--- lesson11-redis/src/app.module.ts
+++ lesson11-redis/src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-yet';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    // CacheModule.registerAsync({
+    //   isGlobal: true,
+    //   useFactory: async (configService: ConfigService) => {
+    //     const redisConfig = configService.get('redis');
+
+    //     return {
+    //       store: await redisStore({
+    //         socket: {
+    //           host: 'localhost',
+    //           port: 6379,
+    //         },
+    //         ttl: 1000,
+    //       }),
+    //     };
+    //   },
+    // }),
+
+    // TODO: 有问题
+    CacheModule.register({
+      useFactory: () => ({
+        store: redisStore,
+        host: 'localhost',
+        port: 6379,
+        // ttl: 1000,
+        // db: 0,
+      }),
+      ttl: 1000,
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- lesson11-redis/src/users/users.service.ts
+++ lesson11-redis/src/users/users.service.ts
@@ -1,22 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async create(createUserDto: CreateUserDto) {
+    /**
+     * 将用户信息存入 redis 缓存
+     */
+    await this.cacheManager.set(
+      `${createUserDto.name}`,
+      createUserDto,
+      1000 * 60 * 1,
+    );
+
+    return {
+      message: 'This action adds a new user',
+      data: createUserDto,
+    };
+  }
+
+  async findUserByName(name: string) {
+    const user = await this.cacheManager.get(name);
+    return {
+      message: 'This action returns a user by name',
+      data: user || '用户不存在',
+    };
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
+    console.log('updateUserDto:', updateUserDto);
     return `This action updates a #${id} user`;
   }
 
